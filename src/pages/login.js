435a08318d80473ce7e74277bb29150207fc33ad
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-03-10 16:30:47 +0300

    fixed validators

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import {useState} from 'react'
 
 import {Container} from './../components/styles/container.styled'
 import {Button} from './../components/styles/button.styled'
 import {CardWrapper, CardBody, CardInput} from './../components/styles/card'
+import {SmallError} from '../components/styles/small_error_message.styled'
+import {LoginFormValidator} from '../validators/login_form_validator'
 import DefaultLayout from './../components/layouts/default_layout'
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
@@ -11,12 +13,12 @@ const eyeOn = <FontAwesomeIcon icon={faEye} />
 const eyeOff = <FontAwesomeIcon icon={faEyeSlash} />
 
 function Login() {
-  const [passwordShown, setPasswordShown] = React.useState(false)
-  const [formState, setFormState] = React.useState({username: '', password: ''})
-
-  React.useEffect(() => {
-    console.log(formState)
-  }, [formState])
+  const [passwordShown, setPasswordShown] = useState(false)
+  const [formValues, setFormValues] = useState({
+    username: '',
+    password: ''
+  })
+  const [formErrors, setFormErrors] = useState({})
 
   const togglePasswordVisiblity = () => {
     setPasswordShown(!passwordShown)
@@ -25,11 +27,16 @@ function Login() {
   function handleEvent(event) {
     const {value, id} = event.target
     if (event.type === 'blur') {
-      //OnBlur
-      setFormState({...formState, [id]: value})
+      setFormValues({...formValues, [id]: value})
     } else if (event.type === 'change') {
-      setFormState({...formState, [id]: value.trim()})
+      setFormValues({...formValues, [id]: value.trim()})
     }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormErrors(LoginFormValidator(formValues))
+    // if (Object.keys(formErrors).length === 0) setIsSubmit(true)
   }
 
   return (
@@ -37,7 +44,7 @@ function Login() {
       <Container>
         <CardWrapper>
           <CardBody>
-            <form>
+            <form onSubmit={() => handleSubmit}>
               <CardInput
                 placeholder="Username"
                 id="username"
@@ -46,6 +53,7 @@ function Login() {
                 onChange={(e) => handleEvent(e)}
                 required
               />
+              <SmallError>{formErrors.username}</SmallError>
               <CardInput
                 placeholder="Password"
                 id="password"
@@ -55,7 +63,10 @@ function Login() {
                 rightSlot={passwordShown ? eyeOn : eyeOff}
                 required
               />
-              <i onClick={togglePasswordVisiblity}>{eyeOn}</i>
+              <SmallError>{formErrors.password}</SmallError>
+              <i onClick={togglePasswordVisiblity}>
+                {passwordShown ? eyeOn : eyeOff}
+              </i>
               <Button bg="#0F9D58" color="#fff" type="submit">
                 Login
               </Button>
