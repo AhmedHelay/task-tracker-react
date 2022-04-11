cebb399c799bc8ba9748ac59cc37bcede6d09581
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-04-11 15:56:54 +0300

    added new pages route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
+
 import {AuthUser} from 'global/AuthUser'
 import EnvSpecificRouter from 'components/EnvSpecificRouter'
+
 import Home from 'pages/home'
 import Login from 'pages/login'
 import Registration from 'pages/registration'
+import Users from 'pages/users'
+import Profile from 'pages/profile'
 
 function App() {
   return (
@@ -11,6 +15,9 @@ function App() {
       <EnvSpecificRouter>
         <Routes>
           <Route path={''} element={<Home />} />
+          <Route exact path={'/'} element={<Home />} />
+          <Route path={'/users'} element={<Users />} />
+          <Route path={'/profile'} element={<Profile />} />
           <Route path={'/login'} element={<Login />} />
           <Route path={'/registration'} element={<Registration />} />
         </Routes>
