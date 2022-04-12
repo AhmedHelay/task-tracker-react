fb839d265413b99bfe46c683aa9c347992e3019f
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-04-12 13:32:00 +0300

    fixed auth validators

--- src/validators/formValidators/registrationFormValidator.js
+++ src/validators/formValidators/registrationFormValidator.js
@@ -2,11 +2,11 @@ import emailValidator from './inputValidators/emailValidator'
 import textFieldValidator from './inputValidators/textFieldValidator'
 import passwordValidator from './inputValidators/passwordValidator'
 
-export default function registrationFormValidator(values) {
+export default function registrationFormValidator(formState, errorsState) {
   const errors = {}
-  errors.firstName = textFieldValidator(values.firstName)
-  errors.lastName = textFieldValidator(values.lastName)
-  errors.email = emailValidator(values.email)
-  errors.password = passwordValidator(values.password)
-  return errors
+  errors.firstName = textFieldValidator(formState.firstName)
+  errors.lastName = textFieldValidator(formState.lastName)
+  errors.email = emailValidator(formState.email)
+  errors.password = passwordValidator(formState.password)
+  return {...errorsState, ...errors}
 }
