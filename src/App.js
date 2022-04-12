b49af7a6eb3c1a23e85ee2498305e1044b06a299
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-04-12 13:33:15 +0300

    add password reovery route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import EnvSpecificRouter from 'components/EnvSpecificRouter'
 import Home from 'pages/home'
 import Login from 'pages/login'
 import Registration from 'pages/registration'
+import PasswordRecovery from 'pages/passwordRecovery'
 import Users from 'pages/users'
 import Profile from 'pages/profile'
 
@@ -18,7 +19,8 @@ function App() {
           <Route exact path={'/'} element={<Home />} />
           <Route path={'/users'} element={<Users />} />
           <Route path={'/profile'} element={<Profile />} />
-          <Route path={'/login'} element={<Login />} />
+          <Route exact path={'/login'} element={<Login />} />
+          <Route path={'/login/recovery'} element={<PasswordRecovery />} />
           <Route path={'/registration'} element={<Registration />} />
         </Routes>
       </EnvSpecificRouter>
