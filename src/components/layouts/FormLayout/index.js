7cfccb431f717a3a88acbf14818b253755c2453f
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-04-12 06:28:07 +0300

    centered form & refactored styles

--- src/components/layouts/FormLayout/index.js
+++ src/components/layouts/FormLayout/index.js
@@ -1,12 +1,31 @@
 import React from 'react'
 
-import {Form, FormTitle, FormInputscontainer} from './components'
+import {
+  Form,
+  FormTitle,
+  FormSubmitMessage,
+  FormPageWrapper,
+  FormInputsContainer
+} from './components'
+import DotLoader from 'react-spinners/DotLoader'
 
-export default function FormLayout({title, children}) {
+export default function FormLayout({title, loading, success, error, children}) {
   return (
-    <Form>
-      <FormTitle>{title}</FormTitle>
-      <FormInputscontainer>{children}</FormInputscontainer>
-    </Form>
+    <FormPageWrapper>
+      {loading ? (
+        <DotLoader color={'#6400f7'} loading={loading} size={50} />
+      ) : (
+        <Form>
+          <FormTitle>{title}</FormTitle>
+          {success && (
+            <FormSubmitMessage color="#5aed73">{success}</FormSubmitMessage>
+          )}
+          {error && (
+            <FormSubmitMessage color="#eb3333">{error}</FormSubmitMessage>
+          )}
+          <FormInputsContainer>{children}</FormInputsContainer>
+        </Form>
+      )}
+    </FormPageWrapper>
   )
 }
