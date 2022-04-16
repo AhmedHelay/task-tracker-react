c6fd74a53778e6ab0b1b056ea98d979737abdd3c
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-04-17 00:41:22 +0300

    center all divs and load relocate loading icon in default layout

--- src/components/layouts/DefaultLayout/components.js
+++ src/components/layouts/DefaultLayout/components.js
@@ -1,6 +1,27 @@
 import styled from 'styled-components'
 
-export const Content = styled.div`
+export const PageWrapper = styled.div`
   background-color: #121212;
   height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`
+
+export const PageLoading = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+
+export const PageContent = styled.div`
+  align-items: start;
+  border-radius: 10px;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  max-height: 100%;
+  margin: 10px;
 `
