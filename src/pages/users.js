85aea8af475bf261d4ad4352b5a26d84af36534c
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-04-18 16:11:54 +0300

    added user cards to users page

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,11 +1,17 @@
 import React, {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
+
 import useAuthUser from 'global/AuthUser'
+import {useQuery} from '@apollo/client'
+import {USERS} from 'api/query/users'
 
 import DefaultLayout from 'components/layouts/DefaultLayout'
+import UsersCardsWrapper from 'components/entity/users/UserCardsWrapper'
+import UserCard from 'components/entity/users/UserCard'
 
 export default function Users() {
   const {user, isLoading} = useAuthUser()
+  const {data, loading} = useQuery(USERS)
 
   const navigate = useNavigate()
   useEffect(() => {
@@ -13,5 +19,21 @@ export default function Users() {
       navigate('/login')
     }
   }, [user, isLoading, navigate])
-  return <DefaultLayout></DefaultLayout>
+
+  return (
+    <DefaultLayout loading={loading}>
+      <UsersCardsWrapper>
+        {data?.users?.map((user) => (
+          <UserCard
+            key={user.id}
+            id={user.id}
+            email={user.email}
+            firstName={user.firstName}
+            lastName={user.lastName}
+            avatarUrl={user.avatarUrl}
+          />
+        ))}
+      </UsersCardsWrapper>
+    </DefaultLayout>
+  )
 }
