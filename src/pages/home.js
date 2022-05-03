49b9e2892c88831191943f47b35bd4b0753510a9
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-05-03 16:49:44 +0300

    reafactor CRUD hooks

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,14 +2,6 @@ import React, {useEffect, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import useAuthUser from 'global/AuthUser'
 
-import useCreateProject from 'hooks/mutations/projects/useCreateProject'
-import useUpdateProject from 'hooks/mutations/projects/useUpdateProject'
-import useDestoryProject from 'hooks/mutations/projects/useDestroyProject'
-import useAddUserToProject from 'hooks/mutations/projects/useAddUserToProject'
-import useCreateTask from 'hooks/mutations/tasks/useCreateTask'
-import useUpdateTask from 'hooks/mutations/tasks/useUpdateTask'
-import useDestoryTask from 'hooks/mutations/tasks/useDestroyTask'
-
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import ProjectsCardsWrapper from 'components/entity/projects/ProjectsWrapper'
 import ProjectCreateForm from 'components/entity/projects/ProjectCreateForm'
@@ -21,15 +13,6 @@ export default function Home() {
   const {user, isLoading} = useAuthUser()
   const [currentOpenProject, setCurrentOpenProject] = useState(undefined)
   const [currentOpenTask, setCurrentOpenTask] = useState(undefined)
-
-  const {createProject} = useCreateProject()
-  const {updateProject} = useUpdateProject()
-  const {destroyProject} = useDestoryProject()
-  const {addUserToProject} = useAddUserToProject()
-
-  const {createTask} = useCreateTask()
-  const {updateTask} = useUpdateTask()
-  const {destroyTask} = useDestoryTask()
 
   function showProject(project) {
     setCurrentOpenProject(project)
@@ -56,26 +39,21 @@ export default function Home() {
             name={project.name}
             tasks={project.tasks}
             onTaskShowClick={showTask}
-            onTaskCreateClick={createTask}
-            onTaskDestroyClick={destroyTask}
             onProjectShowClick={() => showProject(project)}
-            onProjectDestroyClick={() => destroyProject(project.id)}
           />
         ))}
-        <ProjectCreateForm onCreateClick={createProject} />
+        <ProjectCreateForm />
       </ProjectsCardsWrapper>
-      {false && (
+      {currentOpenProject && (
         <ProjectExpandCard
-          onCloseCardClick={setCurrentOpenProject}
           project={currentOpenProject}
-          onAddUserClick={addUserToProject}
-          onUpdateClick={updateProject}
+          onCloseCardClick={setCurrentOpenProject}
         ></ProjectExpandCard>
       )}
-      {false && (
+      {currentOpenTask && (
         <TaskExpandCard
           task={currentOpenTask}
-          onUpdateClick={updateTask}
+          onCloseCardClick={setCurrentOpenTask}
         ></TaskExpandCard>
       )}
     </DefaultLayout>
