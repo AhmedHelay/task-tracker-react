6ac0b7d05432703a196c79c45e0351b9f665926d
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-05-07 13:45:22 +0300

    style user card for project modal

--- src/components/entity/users/UserCard/index.js
+++ src/components/entity/users/UserCard/index.js
@@ -1,31 +1,27 @@
 import React from 'react'
 
+import Box from '@mui/material/Box'
 import Avatar from '@mui/material/Avatar'
-import Grid from '@mui/material/Grid'
 import Tooltip from '@mui/material/Tooltip'
-import {Card, Img, Text} from '../components'
+import Typography from '@mui/material/Typography'
+import {useStyle} from './components'
 
-export default function UserCard({firstName, lastName, email, avatarUrl}) {
+export default function UserCard({firstName, lastName, email}) {
   const shortName = () =>
     firstName && lastName ? firstName.charAt(0) + lastName.charAt(0) : 'A'
 
   const fullName = () =>
     firstName && lastName ? firstName + ' ' + lastName : 'Anonymous'
 
+  const classes = useStyle()
   return (
-    <Card>
-      <Grid container alignItems="center" justifyContent="start">
+    <Box className={classes.root}>
+      <Box className={classes.body}>
         <Tooltip title={fullName()} placement="top" arrow>
-          {avatarUrl ? (
-            <Img src={avatarUrl} />
-          ) : (
-            <Avatar sx={{bgcolor: '#6400f7', width: 50, height: 50}}>
-              {shortName()}
-            </Avatar>
-          )}
+          <Avatar className={classes.avatar}>{shortName()}</Avatar>
         </Tooltip>
-        <Text>{email}</Text>
-      </Grid>
-    </Card>
+        <Typography className={classes.email}>{email}</Typography>
+      </Box>
+    </Box>
   )
 }
